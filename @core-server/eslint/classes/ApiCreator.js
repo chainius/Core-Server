46bef336d0904ec3f7a48aa881bb3397ab7a81bc
sacha vandamme <sacha@skyhark.be>
2021-11-30 22:16:02 +0100

    eslint module

---
+++ @core-server/eslint/classes/ApiCreator.js
@@ -0,0 +1,73 @@
+module.exports = SuperClass
+
+// Load eslint config
+var config
+try {
+    config = require(process.cwd() + '/.eslintrc.js')
+} catch(e) {
+    if(e.code != 'MODULE_NOT_FOUND')
+        console.error('could not load eslint config', e)
+
+    return
+}
+
+// Import packages
+const Linter = require("eslint").Linter
+const { logger, mapper } = require('vite-plugin-eslint-logger/src/logger.js')
+const linter = new Linter({ cwd: process.cwd() })
+const fs = require('fs')
+
+linter.defineParser('@babel/eslint-parser', require('@babel/eslint-parser'))
+
+if(String.prototype.replaceAll === undefined) {
+    String.prototype.replaceAll = function(search, replacement) {
+        return this.split(search).join(replacement)
+    }
+}
+
+// ESLint validator
+function verify(path, data, config) {
+    var filename = path
+    var index = path.lastIndexOf("/")
+    if(index >= 0)
+        filename = filename.substring(index + 1)
+
+    var res = linter.verifyAndFix(data, config, { filename })
+    if(res.fixed && res.output) {
+        fs.writeFileSync(path, res.output)
+        return res.output
+    } else if(res.messages.length == 0) {
+        return data
+    }
+
+    res = mapper({
+        filePath: path,
+        messages: res.messages,
+        source: data,
+    })
+
+    for(var item of res)
+        logger(item)
+
+    throw('eslint failed')
+}
+
+// override api file reader
+SuperClass.fileContent = function(path) {
+    if (SuperClass.fileExists(path)) {
+        var data = ""
+        try {
+            data = fs.readFileSync(path, 'utf8').toString()
+        } catch (e) {
+            console.error('fileContent', e)
+            return false
+        }
+
+        if(path.substr(-3) != ".js")
+            return data
+
+        return verify(path, data, config)
+    }
+
+    return false
+}
